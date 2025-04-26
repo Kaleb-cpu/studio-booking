716e4847a2edd66b7289e95db0c9f3fdeb84e3b9
Kaleb Berhane <kaleb.cpu@gmail.com>
2025-04-26 13:11:46 -0600

    bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { Lora, Quicksand } from "next/font/google";
+import { Quicksand } from "next/font/google";
 
 const quicksand = Quicksand({
   weight: ["400", "600", "700"],
